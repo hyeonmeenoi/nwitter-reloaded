84c2a42313e1eb9de6c606ba7e8217e992e75c2a
Hyeonmin Kwon <hyeonmeenoi@gmail.com>
2023-09-13 11:24:49 +0900

    Avatar

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: "home",
+        path: "",
         element: <Home />,
       },
       {
--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -7,7 +7,7 @@ export default function ProtectedRoute({
   children: React.ReactNode;
 }) {
   const user = auth.currentUser;
-  if (!user) {
+  if (user === null) {
     return <Navigate to="/login" />;
   }
   return children;
